ba4d7357db0a76e23f2871f95d5c57cf4bc03708
Ryan Hellwege <rhellwege6@gmail.com>
2026-05-19 18:23:49 -0700

    add workflow

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,42 @@ use crate::{
     piece::Piece,
 };
 
+struct Cross {
+    color: Color,
+}
+
+impl Cross {
+    fn new(color: Color) -> Self {
+        Self { color }
+    }
+}
+
+impl Piece for Cross {
+    fn name(&self) -> &str {
+        "Cross"
+    }
+
+    fn color(&self) -> Color {
+        self.color
+    }
+
+    fn move_offsets(&self) -> &[(i32, i32)] {
+        &[(-1, 0), (1, 0), (0, -1), (0, 1)]
+    }
+}
+
 fn main() -> std::io::Result<()> {
-    let mapper = CenterSpiralMapper::new(10000, false);
+    // let mapper = CenterSpiralMapper::new(1000, false);
     // let mapper = HilbertMapper::new(8);
-    // let mapper = CenterHilbertMapper::new(7);
+    let mapper = CenterSpiralMapper::new(400, false);
     // let piece = Knight::new(Color::from_u32(0x00ff00ff));
     let pieces: Vec<Box<dyn Piece>> = vec![
-        Box::new(Knight::new(Color::from_u32(0x000000ff))),
-        Box::new(Knight::new(Color::from_u32(0xff0000ff))),
-        // Box::new(Knight::new(Color::from_u32(0x00ff00ff))),
         // Box::new(Knight::new(Color::from_u32(0x0000ffff))),
+        Box::new(Cross::new(Color::from_u32(0xff0000ff))),
+        Box::new(Knight::new(Color::from_u32(0x00ff00ff))),
+        Box::new(Knight::new(Color::from_u32(0xffff00ff))),
+        Box::new(Knight::new(Color::from_u32(0x0000ffff))),
+        // Box::new(Knight::new(Color::from_u32(0xffff00ff))),
     ];
 
     let tour = DuelingKnights::new(pieces, mapper);
